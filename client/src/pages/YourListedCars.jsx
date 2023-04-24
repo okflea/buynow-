a0f3f7bf888af354fb2804dd0b295dea90a603e0
okflea <god@no.ob>
2023-04-24 15:38:21 +0530

    removied add

--- client/src/pages/YourListedCars.jsx
+++ client/src/pages/YourListedCars.jsx
@@ -32,13 +32,13 @@ const YourListedCars = () => {
       <p>{`Hello ${user.firstName} , Welcome !!`}</p>
       <p>You can make changes to your Car listings here</p>
 
-      <button 
-        onClick={() => {
-          navigate('/add')
-        }}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Add New listing
-      </button>
+      {/* <button  */}
+      {/*   onClick={() => { */}
+      {/*     navigate('/add') */}
+      {/*   }} */}
+      {/*   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> */}
+      {/* Add New listing */}
+      {/* </button> */}
       <div className="flex flex-row justify-center items-center gap-2 flex-wrap p-5 mb-16">
         {data.map((item) => (
           <Item edit="true" key={item._id} data={item} />
