aac2ef29f4f5377476c7b9876d3d52022fea24db
okflea <god@no.ob>
2023-05-02 23:15:09 +0530

    footer and nav

--- client/src/components/Foot.jsx
+++ client/src/components/Foot.jsx
@@ -1,32 +1,71 @@
-import { Footer } from "flowbite-react"
-import { BsTwitter, BsGithub } from "react-icons/bs"
+import React from "react";
+import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
 
-const Foot = () => {
+const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-screen">
-      <Footer container={true}>
-        <div className="w-full">
-          <div className="w-full sm:flex sm:items-center sm:justify-between">
-            <Footer.Copyright
-              href="#"
-              by="BuyNow™"
-              year={2022}
-            />
-            <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-              <Footer.Icon
-                href="#"
-                icon={BsTwitter}
-              />
-              <Footer.Icon
-                href="#"
-                icon={BsGithub}
-              />
-            </div>
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="text-sm">&copy; 2023 Buynow!. All rights reserved.</p>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="#" className="text-gray-400 hover:text-white transition duration-150 ease-in-out">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-400 hover:text-white transition duration-150 ease-in-out">
+                  Terms of Service
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex space-x-4">
+            <a href="#" className="text-gray-400 hover:text-white transition duration-150 ease-in-out">
+              <AiFillGithub size={24} />
+            </a>
+            <a href="#" className="text-gray-400 hover:text-white transition duration-150 ease-in-out">
+              <AiFillTwitterCircle size={24} />
+            </a>
           </div>
         </div>
-      </Footer>
-    </div>
-  )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Foot
+export default Footer;
+
+// import { Footer } from "flowbite-react"
+// import { BsTwitter, BsGithub } from "react-icons/bs"
+
+// const Foot = () => {
+//   return (
+//     <div className="fixed bottom-0 w-screen">
+//       <Footer container={true}>
+//         <div className="w-full">
+//           <div className="w-full sm:flex sm:items-center sm:justify-between">
+//             <Footer.Copyright
+//               href="#"
+//               by="BuyNow™"
+//               year={2022}
+//             />
+//             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+//               <Footer.Icon
+//                 href="#"
+//                 icon={BsTwitter}
+//               />
+//               <Footer.Icon
+//                 href="#"
+//                 icon={BsGithub}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </Footer>
+//     </div>
+//   )
+// }
+
+// export default Foot
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,60 +1,164 @@
 import { UserButton, useUser } from "@clerk/clerk-react"
-import { Navbar } from "flowbite-react"
-import React from "react"
-import { useNavigate } from "react-router-dom"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
-  const navigate = useNavigate()
-  const path = window.location.pathname
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const { user } = useUser()
+  const handleNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
 
   return (
-    <div >
-      <Navbar
-        fluid={true}
-      // rounded={true}
-      >
-        <Navbar.Brand
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-slate-800 whitespace-nowrap text-xl font-semibold dark:text-slate-200">
-            BuyNow!
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link
-            onClick={() => navigate("/")}
-            active={path === "/"}
-          >
-            Home
-          </Navbar.Link>
-          <Navbar.Link
-            active={path === "/cars"}
-            onClick={() => navigate("/cars")}
-          >
-            Cars
-          </Navbar.Link>
-          <Navbar.Link
-            active={path === "/your-listed-cars"}
-            onClick={() => navigate("/your-listed-cars")}
-          >
-            Your Listed Cars
-          </Navbar.Link>
+    <nav className="bg-gray-800 shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link to="/" className="text-white font-bold text-xl">
+              BuyNow!
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                to="/"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </Link>
+              <Link
+                to="/cars"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Cars
+              </Link>
+              <Link
+                to="/your-listed-cars"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Your Listings
+              </Link>
           {user ? <UserButton /> :
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => navigate("/login")}>
+            <Link
+              to="/login"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
               Sign In
-            </button>
+            </Link>
           }
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  )
-}
+            </div>
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={handleNavToggle}
+              className="text-gray-400 hover:text-white focus:outline-none focus:text-white"
+              aria-label="Toggle menu"
+            >
+              {isNavOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`${
+            isNavOpen ? "block" : "hidden"
+          } md:hidden transition-all ease-in-out duration-300`}
+        >
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/products"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Cars
+            </Link>
+            <Link
+              to="/your-listed-cars"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Your Listings
+            </Link>
+          {user ? <UserButton /> :
+            <Link
+              to="/login"
+              className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Sign In
+            </Link>
+          }
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
+
+// import { UserButton, useUser } from "@clerk/clerk-react"
+// import { Navbar } from "flowbite-react"
+// import React from "react"
+// import { useNavigate } from "react-router-dom"
+
+
+// const NavBar = () => {
+//   const navigate = useNavigate()
+//   const path = window.location.pathname
+//   const { user } = useUser()
+
+//   return (
+//     <div >
+//       <Navbar
+//         fluid={true}
+//         dark={true}
+//       // rounded={true}
+//       >
+//         <Navbar.Brand
+//         >
+//           <img
+//             src="https://flowbite.com/docs/images/logo.svg"
+//             className="mr-3 h-6 sm:h-9"
+//             alt="Flowbite Logo"
+//           />
+//           <span className="self-center text-slate-800 whitespace-nowrap text-xl font-semibold dark:text-slate-200">
+//             BuyNow!
+//           </span>
+//         </Navbar.Brand>
+//         <Navbar.Toggle />
+//         <Navbar.Collapse>
+//           <Navbar.Link
+//             onClick={() => navigate("/")}
+//             active={path === "/"}
+//           >
+//             Home
+//           </Navbar.Link>
+//           <Navbar.Link
+//             active={path === "/cars"}
+//             onClick={() => navigate("/cars")}
+//           >
+//             Cars
+//           </Navbar.Link>
+//           <Navbar.Link
+//             active={path === "/your-listed-cars"}
+//             onClick={() => navigate("/your-listed-cars")}
+//           >
+//             Your Listed Cars
+//           </Navbar.Link>
+//           {user ? <UserButton /> :
+//             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => navigate("/login")}>
+//               Sign In
+//             </button>
+//           }
+//         </Navbar.Collapse>
+//       </Navbar>
+//     </div>
+//   )
+// }
+
+// export default NavBar
